08dde2d5b7538ee80f60e069654b7f8d4f2ff0b5
vasquezcruz532 <vasquezcruz532@gmail.com>
2025-11-26 20:40:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import sys
 # TODO: importar la función reloj_arena
-from solucion import [NOMBRE_DE_LA_FUNCION]
+from solucion import reloj_arena
 
 def main():
     """
@@ -28,13 +28,15 @@ def main():
 
     # Intentar convertir la altura a entero
     try:
+        m = int(m_str)
         # TODO: Convertir m_str a entero y asignarlo a m
-        pass
     except ValueError:
+        print("Error: La altura debe ser un numero entero")
         # TODO: imprimir "Error: La altura debe ser un numero entero" y salir
-        pass
+        return
 
     # TODO: llamar a la función reloj_arena con los parámetros m y s
+    reloj_arena(m,s)
 
 if __name__ == "__main__":
     main()
